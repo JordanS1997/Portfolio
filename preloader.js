4e0c5031e7a926fcd068e907127f3113b5f467cd
JordanS1997 <128438643+JordanS1997@users.noreply.github.com>
2024-05-04 20:25:59 -0400

    added changes to preloader and carousel

--- preloader.js
+++ preloader.js
@@ -1,6 +1,11 @@
+
 var loader = document.getElementById('preloader');
 
-window.addEventListener('load', function(){
-    loader.style.display = 'none';
-})
+// window.addEventListener('load', function(){
+//     loader.style.display = 'none';
+// })
 
+
+setTimeout(function(){
+    document.querySelector('#preloader').classList.add('fade-out');
+}, 3000);
